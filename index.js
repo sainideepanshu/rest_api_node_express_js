88b8c258df205cdc741c336d13f82440f81e5aa8
deepanshu <deepanshukamlesh@gamil.com>
2024-05-08 22:17:53 +0530

    get post patch delete route added

--- index.js
+++ index.js
@@ -29,6 +29,33 @@ app.get("/api/users",(req,res) => {
     return res.json(users);
 });
 
+
+app.get("/api/users/:id",(req,res) => {
+
+    const id = Number(req.params.id);
+
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+});
+
+app.post("/api/users",(req,res) => {
+
+    //TODO : Create new user
+    return res.json({status : "pending"});
+});
+
+app.patch("/api/users/:id",(req,res) => {
+
+    //TODO : Edit the user with id
+    return res.json({status : "pending"});
+});
+
+app.delete("/api/users/:id",(req,res) => {
+
+    //TODO : Delete the user with id
+    return res.json({status : "pending"});
+});
+
 app.listen(PORT,() => {
 
     console.log(`Server started at PORT ${PORT}`);
