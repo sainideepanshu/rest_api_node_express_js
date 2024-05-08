f0265333061b0cb9eee4be5858daeb6d6b5decf4
deepanshu <deepanshukamlesh@gamil.com>
2024-05-08 22:23:10 +0530

    refactored patch,delete and get routes

--- index.js
+++ index.js
@@ -29,30 +29,30 @@ app.get("/api/users",(req,res) => {
     return res.json(users);
 });
 
+app
+    .route("/api/users/:id")
+    .get((req,res) => {
 
-app.get("/api/users/:id",(req,res) => {
+        const id = Number(req.params.id);
+    
+        const user = users.find((user) => user.id === id);
+        return res.json(user);
+    })
+    .patch((req,res) => {
 
-    const id = Number(req.params.id);
+        //TODO : Edit the user with id
+        return res.json({status : "pending"});
+    })
+    .delete((req,res) => {
 
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-});
+        //TODO : Delete the user with id
+        return res.json({status : "pending"});
+    });
+
 
 app.post("/api/users",(req,res) => {
 
     //TODO : Create new user
-    return res.json({status : "pending"});
-});
-
-app.patch("/api/users/:id",(req,res) => {
-
-    //TODO : Edit the user with id
-    return res.json({status : "pending"});
-});
-
-app.delete("/api/users/:id",(req,res) => {
-
-    //TODO : Delete the user with id
     return res.json({status : "pending"});
 });
 
