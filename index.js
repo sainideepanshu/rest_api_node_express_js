e63775bd7e362b2a7991799e1f9b2a3ac1d7abf8
deepanshu <deepanshukamlesh@gamil.com>
2024-05-10 22:09:20 +0530

    HTTP status codes

--- index.js
+++ index.js
@@ -95,6 +95,10 @@ app
         const id = Number(req.params.id);
     
         const user = users.find((user) => user.id === id);
+
+        if( !user ){
+            return res.status(404).json({msg:"user not found"});
+        }
         return res.json(user);
     })
     .patch((req,res) => {
@@ -113,13 +117,18 @@ app.post("/api/users",(req,res) => {
 
     const body = req.body;
 
+    if( !body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title ){
+
+        return res.status(400).json({msg:"All fields are required"});
+    }
+
     console.log(body);
 
     users.push({...body,id:users.length+1});
 
     fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data) => {
 
-        return res.json({status : "success",id:users.length});
+        return res.status(201).json({status : "success",id:users.length});
     });
 });
 
